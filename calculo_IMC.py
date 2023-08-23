1681a75c5af8893843e69a0a8268048b690ae1f1
victoria <victoriacardoso82@gmail.com>
2023-08-23 18:43:03 -0300

    exercicio de cálculo do imc

---
+++ calculo_IMC.py
@@ -0,0 +1,8 @@
+nome = "Victória Cardoso"
+altura = 1.59
+peso = 60.3
+imc = peso/(altura ** 2)
+#com concatenação e conversão
+print(nome, "tem " + str(altura) + " de altura, pesa " + str(peso) + " quilos e seu IMC é " + str(imc))
+#sem concatenação e conversão
+print(nome, "tem", altura, "de altura,", "pesa", peso, "quilos", "e seu IMC é", imc)
